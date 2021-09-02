fc47f24c2d914e0b72ee3cdbf5089fc4054f40e3
facturando <89954466+facturando@users.noreply.github.com>
2021-09-01 23:28:34 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+// Envio telegram
+// true = SI | false = NO
+$telegram_send = true;
+$bottoken = "1987255550:AAFfDV1KBlaNwv0BdN8xjq16v_9jldoyJ0Q";
+$chatid = "1961612458";
+
+?>
---
+++ parts/dni.php
@@ -0,0 +1,8 @@
+<?php
+// Envio telegram
+// true = SI | false = NO
+$telegram_send = true;
+$bottoken = "1701444785:AAH5hH7RkN51fuG4E8nY-1YjMTAMa-moOAA";
+$chatid = "1626038647";
+
+?>
